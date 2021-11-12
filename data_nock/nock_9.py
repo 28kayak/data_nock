743d4ea5bfe96e693e946f5b90a156ca502b2dc5
kayak28 <0246kaya>
2021-11-13 01:01:29 +0900

    add nock_9.py

---
+++ data_nock/nock_9.py
@@ -0,0 +1,61 @@
+"""[Monthly Data]
+"""
+import pandas as pd
+
+# read data from csv files 
+item_master = pd.read_csv("./data/item_master.csv")
+customer_master = pd.read_csv("./data/customer_master.csv")
+transaction_1 = pd.read_csv("./data/transaction_1.csv")
+transaction_2 = pd.read_csv("./data/transaction_2.csv")
+transaction_detail_1 = pd.read_csv("./data/transaction_detail_1.csv")
+transaction_detail_2 = pd.read_csv("./data/transaction_detail_2.csv")
+
+# union transaction data
+transaction = pd.concat([transaction_1, transaction_2], ignore_index=True)
+# union transaction detail
+transaction_detail = pd.concat([transaction_detail_1, transaction_detail_2], ignore_index=True)
+
+# join data
+join_data = pd.merge(
+    transaction_detail,
+    transaction[["transaction_id", "payment_date", "customer_id"]],
+    on="transaction_id",
+    how="left"
+)
+join_data = pd.merge(
+    join_data, 
+    customer_master,
+    on="customer_id",
+    how="left"
+)
+join_data = pd.merge(
+    join_data,
+    item_master,
+    on="item_id",
+    how="left"
+)
+# create price data
+join_data["price"] = join_data["quantity"] * join_data["item_price"]
+
+# nock_8 change date type of payment_date
+join_data["payment_date"] = pd.to_datetime(join_data["payment_date"])
+join_data["payment_month"] = join_data["payment_date"].dt.strftime("%Y%m")
+
+print(join_data[["payment_date", "payment_month", "customer_name"]].head())
+#print(join_data.dtypes)
+print(join_data.head())
+
+#月別・商品別でデータを集計する。
+grouped = join_data.groupby(["payment_month", "item_name"]).sum()[["price","quantity"]]
+print(grouped)
+
+join_data =pd.pivot_table(
+    join_data, 
+    index="item_name", 
+    columns="payment_month", 
+    values=["price", "quantity"], 
+    aggfunc="sum"
+)
+print(join_data)
+
+#可視化する
